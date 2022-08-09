043510a01a3fabee0af008ecb42f496174aa444c
Eray Nurtekin <79145084+eraynurtekin@users.noreply.github.com>
2022-08-09 20:13:56 +0300

    Props kavramları üzerine çalışma

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM  from 'react-dom'
+import SingleComment from './SingleComment'
+import profile1 from './image/sarah.jpg'
+import profile2 from './image/John.jfif'
+import profile3 from './image/Peter.jfif'
+import UserCard from './UserCard'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () =>{
+  return(
+    <div className='ui comments'>
+       <UserCard>
+       <SingleComment 
+       name='Sarah' 
+       date='Today at 5:00PM'
+       text ='It is amazing'
+       picture={profile1}
+       />
+       </UserCard>
+       <UserCard>
+       <SingleComment 
+       name='John' 
+       date='Today at 6:00PM'
+       text ='It is cool'
+       picture={profile2}/>
+       </UserCard>
+       <UserCard>
+       <SingleComment 
+       name='Peter' 
+       date='Today at 7:00PM'
+       text ='It is not okay'
+       picture={profile3}/>
+       </UserCard>
+       
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+)
